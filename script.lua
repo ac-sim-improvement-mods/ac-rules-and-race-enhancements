d4647dcdaaed49716d74985c11f1a2ce5c8e5d3d
William Gawlik <william.gawlik@outlook.com>
2022-08-29 21:57:19 -0500

    Fixed car launch control

--- script.lua
+++ script.lua
@@ -1,32 +1,78 @@
 ---
 --- Script v0.8.0-alpha
 ---
+local OFF_TRACK_TIMER = 0
+
 if not car.isAIControlled then
     return nil
 end
 
-function temp_drag_reduciton()
-    local data = ac.accessCarPhysics()
+function temp_drag_reduction(data)
     data.brake = 0.1001
-    --ac.setWingGain(9, 15, 1)
+
+    if car.speedKmh < 100 then
+        ac.setWingGain(9, 250, 1)
+    elseif car.speedKmh < 150 then
+        ac.setWingGain(9, 200, 1)
+    elseif car.speedKmh < 275 then
+        ac.setWingGain(9, 30, 1)
+    else
+        ac.setWingGain(9, 1, 1)
+    end
 end
 
 function temp_drag_reset()
-    --ac.setWingGain(9, 1, 1)
+    ac.setWingGain(9, 1, 1)
+end
+
+function car_launch(data, launch)
+    local rear_slip = data.wheels[2].ndSlip + data.wheels[3].ndSlip / 2
+    if data.brake <= 0.1001 then
+        
+        if launch then
+            data.gas = math.clamp(1/math.clamp((rear_slip-0.5),1,4),0,1)
+            data.steer = math.clamp(data.steer, -0.05, 0.05)
+        else
+            car.gear = 2
+            data.gas = math.clamp(1/(rear_slip*2),0,0.5)
+        end
+    end
 end
 
 function script.update(dt)
-    if sim.raceSessionType == 3 then
-        local drs_available = stringify.parse(ac.load("F1Reg"))[car.index..".drsAvailable"]
+    local data = ac.accessCarPhysics()
+    local sim = ac.getSim()
 
-        if drs_available == false then
-            if car.drsActive then
-                temp_drag_reduciton()
+    if sim.raceSessionType == 3 then
+        if sim.isSessionStarted then
+            local drs_available = stringify.parse(ac.load("F1Reg"))[car.index..".drsAvailable"]
+
+            if drs_available == false then
+                if car.drsActive then
+                    if sim.timeToSessionStart < -5000 and data.brake <= 0.1001 then
+                        data.gas = 1
+                    end
+                    temp_drag_reduction(data)
+                else
+                    temp_drag_reset()
+                end
             else
                 temp_drag_reset()
             end
-        else
-            temp_drag_reset()
+
+            if car.wheelsOutside == 4 then
+                OFF_TRACK_TIMER = OFF_TRACK_TIMER+1
+                if OFF_TRACK_TIMER > 3000 then
+                    
+                    car_launch(data, false)
+                end
+            else
+                OFF_TRACK_TIMER = 0
+            end
+
+            if sim.timeToSessionStart > -5000 then
+                car_launch(data, true)
+            end
         end
     end
 end
