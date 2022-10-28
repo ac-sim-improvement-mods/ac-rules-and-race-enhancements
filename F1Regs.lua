be7813367fea80536343f5cfb7bde54d5bece85c
William Gawlik <william.gawlik@outlook.com>
2022-10-27 23:30:42 -0500

    Update build script

--- F1Regs.lua
+++ F1Regs.lua
@@ -1,4 +1,4 @@
-local SCRIPT_VERSION = "0.9.7.0-alpha"
+local SCRIPT_VERSION = "0.9.7.1-alpha"
 local SCRIPT_VERSION_ID = 0970
 local SCRIPT_RELEASE_DATE = "2022-10-27"
 
--- build.ps1
+++ build.ps1
@@ -1,8 +1,25 @@
 $build_dir = "$PSScriptRoot\build"
+$build_ver = ((Get-Content $PSScriptRoot\F1Regs.lua -First 1) -split "`"")[1]
+$app_dir = "$build_dir\apps\lua\F1Regs"
 
-Copy-Item "$PSScriptRoot\F1Regs.lua" -Destination "$build_dir\apps\lua\F1Regs" -Force
-Copy-Item "$PSScriptRoot\manifest.ini" -Destination "$build_dir\apps\lua\F1Regs" -Force
-Copy-Item "$PSScriptRoot\settings.ini" -Destination "$build_dir\apps\lua\F1Regs" -Force
-Copy-Item "$PSScriptRoot\icon.png" -Destination "$build_dir\apps\lua\F1Regs" -Force
-Copy-Item "$PSScriptRoot\assets" -Destination "$build_dir\apps\lua\F1Regs" -Recurse -Force
-Copy-Item "$PSScriptRoot\data" -Destination "$build_dir\apps\lua\F1Regs" -Recurse -Force
+(Get-Content "$PSScriptRoot\manifest.ini") | ForEach-Object { $_ -replace "VERSION =.+","VERSION = $build_ver"  } | Set-Content "$PSScriptRoot\manifest.ini"
+
+if (!(Test-Path $app_dir))
+{
+New-Item -itemType Directory -Path $app_dir
+}
+
+Copy-Item "$PSScriptRoot\extension" -Destination $build_dir -Recurse -Force
+Copy-Item "$PSScriptRoot\assets" -Destination $app_dir -Recurse -Force
+Copy-Item "$PSScriptRoot\data" -Destination $app_dir -Recurse -Force
+Copy-Item "$PSScriptRoot\F1Regs.lua" -Destination $app_dir -Force
+Copy-Item "$PSScriptRoot\manifest.ini" -Destination $app_dir -Force
+Copy-Item "$PSScriptRoot\settings.ini" -Destination $app_dir -Force
+Copy-Item "$PSScriptRoot\icon.png" -Destination $app_dir -Force
+
+$target_file = "$build_dir\F1Regs_$build_ver.zip"
+if (Test-Path $target_file) {
+  Remove-Item $target_file
+}
+
+Compress-Archive -Path $build_dir\* -DestinationPath $target_file
