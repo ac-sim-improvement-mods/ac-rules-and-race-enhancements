16b981fb77448a13c01743013b9585d27cd8d474
William Gawlik <william.gawlik@outlook.com>
2022-10-27 20:01:17 -0500

    Updated build.ps1 for 0.9.7.0

--- build.ps1
+++ build.ps1
@@ -1,18 +1,8 @@
 $build_dir = "$PSScriptRoot\build"
 
-Copy-Item "$PSScriptRoot\F1Regs.lua" -Destination "$build_dir\apps\lua\F1Regs"
-Copy-Item "$PSScriptRoot\manifest.ini" -Destination "$build_dir\apps\lua\F1Regs"
-Copy-Item "$PSScriptRoot\settings.ini" -Destination "$build_dir\apps\lua\F1Regs"
-Copy-Item "$PSScriptRoot\icon.png" -Destination "$build_dir\apps\lua\F1Regs"
-Copy-Item "$PSScriptRoot\assets" -Destination "$build_dir\apps\lua\F1Regs" -Recurse
-
-$folders = Get-ChildItem "$build_dir\content\cars"
-
-foreach ($folder in $folders.name){
-$data_dir = "$build_dir\content\cars\$folder\data"
-if (!(test-path -PathType container $data_dir)) {
-    New-Item $data_dir -ItemType Directory
-}
-
-Copy-Item -Path "$PSScriptRoot\script.lua" -Destination "$build_dir\content\cars\$folder\data" -Recurse
-}
+Copy-Item "$PSScriptRoot\F1Regs.lua" -Destination "$build_dir\apps\lua\F1Regs" -Force
+Copy-Item "$PSScriptRoot\manifest.ini" -Destination "$build_dir\apps\lua\F1Regs" -Force
+Copy-Item "$PSScriptRoot\settings.ini" -Destination "$build_dir\apps\lua\F1Regs" -Force
+Copy-Item "$PSScriptRoot\icon.png" -Destination "$build_dir\apps\lua\F1Regs" -Force
+Copy-Item "$PSScriptRoot\assets" -Destination "$build_dir\apps\lua\F1Regs" -Recurse -Force
+Copy-Item "$PSScriptRoot\data" -Destination "$build_dir\apps\lua\F1Regs" -Recurse -Force
