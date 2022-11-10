fb18ce4ccbf27a84e9ec947c1d021c109bff9c0b
William Gawlik <william.gawlik@outlook.com>
2022-11-10 08:16:10 -0600

    Update build date in build script now

--- _build.ps1
+++ _build.ps1
@@ -2,9 +2,10 @@ $build_dir = "$PSScriptRoot\build"
 $build_ver = ((Get-Content $PSScriptRoot\assettocorsa\apps\lua\F1Regs\F1Regs.lua)[0] -split "`"")[1]
 $build_code = ((Get-Content $PSScriptRoot\assettocorsa\apps\lua\F1Regs\F1Regs.lua)[1] -split "= ")[1]
 
-Write-Output $build_code
+$date = Get-Date -Format "yyyy-MM-dd"
 
 (Get-Content "$PSScriptRoot\assettocorsa\apps\lua\F1Regs\manifest.ini") | ForEach-Object { $_ -replace "VERSION =.+","VERSION = $build_ver"  } | Set-Content "$PSScriptRoot\assettocorsa\apps\lua\F1Regs\manifest.ini"
+(Get-Content "$PSScriptRoot\assettocorsa\apps\lua\F1Regs\F1Regs.lua") | ForEach-Object { $_ -replace "SCRIPT_BUILD_DATE =.+","SCRIPT_BUILD_DATE = `"$date`""  } | Set-Content "$PSScriptRoot\assettocorsa\apps\lua\F1Regs\F1Regs.lua"
 
 $target_file = "$build_dir\F1Regs_$build_ver.zip"
 if (Test-Path $target_file) {
