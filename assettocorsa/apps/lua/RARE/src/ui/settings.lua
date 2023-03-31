4f34f2405263bc2796ab89c654ff5cd7f14818a5
William Gawlik <william.gawlik@outlook.com>
2023-03-30 21:48:43 -0500

    Disabled certain settings while not in the main menu

--- assettocorsa/apps/lua/RARE/src/ui/settings.lua
+++ assettocorsa/apps/lua/RARE/src/ui/settings.lua
@@ -53,329 +53,342 @@ function settingsMenu(sim)
 	end
 
 	ui.tabBar("settingstabbar", ui.TabBarFlags.None, function()
-		ui.tabItem("RULES", ui.TabItemFlags.None, function()
-			ui.newLine(1)
-			ui.header("DRS")
-			slider(
-				RARECONFIG,
-				"RULES",
-				"DRS_RULES",
-				0,
-				1,
-				1,
-				true,
-				RARECONFIG.data.RULES.DRS_RULES == 1 and "DRS Rules: ENABLED" or "DRS Rules: DISABLED",
-				"Enable DRS being controlled by the app",
-				function(v)
-					return math.round(v, 0)
-				end
-			)
-
-			if RARECONFIG.data.RULES.DRS_RULES == 1 then
-				DRS_ENABLED_LAP = slider(
-					RARECONFIG,
-					"RULES",
-					"DRS_ACTIVATION_LAP",
-					1,
-					5,
-					1,
-					false,
-					"Activation Lap: %.0f",
-					"First lap to allow DRS activation",
-					function(v)
-						return v
-					end
-				)
-				slider(
-					RARECONFIG,
-					"RULES",
-					"DRS_GAP_DELTA",
-					100,
-					2000,
-					1,
-					false,
-					"Gap Delta: %.0f ms",
-					"Max gap to car ahead when crossing detection line to allow DRS for the next zone",
-					function(v)
-						return math.floor(v / 50 + 0.5) * 50
-					end
-				)
-
+		if ac.getSim().isInMainMenu then
+			ui.tabItem("RULES", ui.TabItemFlags.None, function()
 				ui.newLine(1)
-
+				ui.header("DRS")
 				slider(
 					RARECONFIG,
 					"RULES",
-					"DRS_WET_DISABLE",
+					"DRS_RULES",
 					0,
 					1,
 					1,
 					true,
-					RARECONFIG.data.RULES.DRS_WET_DISABLE == 1 and "Disable DRS When Wet: ENABLED"
-						or "Disable DRS When Wet: DISABLED",
-					"Disable DRS activation if track wetness gets above the limit below",
+					RARECONFIG.data.RULES.DRS_RULES == 1 and "DRS Rules: ENABLED" or "DRS Rules: DISABLED",
+					"Enable DRS being controlled by the app",
 					function(v)
 						return math.round(v, 0)
 					end
 				)
-			end
-			ui.newLine(1)
 
-			-- ui.header("VSC:")
-			-- slider(RARECONFIG, 'RULES', 'VSC_RULES', 0, 1, 1, true, RARECONFIG.data.RULES.VSC_RULES == 1 and "VSC Rules: ENABLED" or "VSC Rules: DISABLED",
-			-- 'Enable a Virtual Safety Car to be deployed',
-			-- function (v) return math.round(v, 0) end)
-			-- if RARECONFIG.data.RULES.VSC_RULES == 1 then
-			--     slider(RARECONFIG, 'RULES', 'VSC_INIT_TIME', 0, 300, 1, false, 'Call After Yellow Flag For: %.0f s',
-			--     'Time a yellow flag must be up before calling the VSC',
-			--     function (v) return math.round(v, 0) end)
-			--     slider(RARECONFIG, 'RULES', 'VSC_DEPLOY_TIME', 0, 300, 1, false, 'Ends After Deployed For: %.0f s',
-			--     'Time that the VSC is deployed before ending',
-			--     function (v) return math.round(v, 0) end)
-			-- end
-			-- ui.newLine(1)
+				if RARECONFIG.data.RULES.DRS_RULES == 1 then
+					DRS_ENABLED_LAP = slider(
+						RARECONFIG,
+						"RULES",
+						"DRS_ACTIVATION_LAP",
+						1,
+						5,
+						1,
+						false,
+						"Activation Lap: %.0f",
+						"First lap to allow DRS activation",
+						function(v)
+							return v
+						end
+					)
+					slider(
+						RARECONFIG,
+						"RULES",
+						"DRS_GAP_DELTA",
+						100,
+						2000,
+						1,
+						false,
+						"Gap Delta: %.0f ms",
+						"Max gap to car ahead when crossing detection line to allow DRS for the next zone",
+						function(v)
+							return math.floor(v / 50 + 0.5) * 50
+						end
+					)
 
-			ui.header("PIT STOPS")
-			slider(
-				RARECONFIG,
-				"RULES",
-				"RACE_REFUELING",
-				0,
-				1,
-				1,
-				true,
-				RARECONFIG.data.RULES.RACE_REFUELING == 1 and "Race Refueling: ENABLED" or "Race Refueling: DISABLED",
-				"Enable or disable refueling during a race",
-				function(v)
-					return math.round(v, 0)
+					ui.newLine(1)
+
+					slider(
+						RARECONFIG,
+						"RULES",
+						"DRS_WET_DISABLE",
+						0,
+						1,
+						1,
+						true,
+						RARECONFIG.data.RULES.DRS_WET_DISABLE == 1 and "Disable DRS When Wet: ENABLED"
+							or "Disable DRS When Wet: DISABLED",
+						"Disable DRS activation if track wetness gets above the limit below",
+						function(v)
+							return math.round(v, 0)
+						end
+					)
 				end
-			)
+				ui.newLine(1)
 
-			ui.newLine(1)
-			-- ui.newLine(5)
+				-- ui.header("VSC:")
+				-- slider(RARECONFIG, 'RULES', 'VSC_RULES', 0, 1, 1, true, RARECONFIG.data.RULES.VSC_RULES == 1 and "VSC Rules: ENABLED" or "VSC Rules: DISABLED",
+				-- 'Enable a Virtual Safety Car to be deployed',
+				-- function (v) return math.round(v, 0) end)
+				-- if RARECONFIG.data.RULES.VSC_RULES == 1 then
+				--     slider(RARECONFIG, 'RULES', 'VSC_INIT_TIME', 0, 300, 1, false, 'Call After Yellow Flag For: %.0f s',
+				--     'Time a yellow flag must be up before calling the VSC',
+				--     function (v) return math.round(v, 0) end)
+				--     slider(RARECONFIG, 'RULES', 'VSC_DEPLOY_TIME', 0, 300, 1, false, 'Ends After Deployed For: %.0f s',
+				--     'Time that the VSC is deployed before ending',
+				--     function (v) return math.round(v, 0) end)
+				-- end
+				-- ui.newLine(1)
 
-			-- if ui.button("APPLY SETTINGS", vec2(ui.windowWidth()-40,25), ui.ButtonFlags.None) then
-			--     -- Load config file
-			--     RARECONFIG = MappedConfig(ac.getFolder(ac.FolderID.ACApps).."/lua/RARE/settings.ini", {
-			--         RULES = { DRS_RULES = ac.INIConfig.OptionalNumber, DRS_ACTIVATION_LAP = ac.INIConfig.OptionalNumber,
-			--         DRS_GAP_DELTA = ac.INIConfig.OptionalNumber, DRS_WET_DISABLE = ac.INIConfig.OptionalNumber,
-			--         VSC_RULES = ac.INIConfig.OptionalNumber, VSC_INIT_TIME = ac.INIConfig.OptionalNumber, VSC_DEPLOY_TIME = ac.INIConfig.OptionalNumber,
-			--         AI_FORCE_PIT_TYRES = ac.INIConfig.OptionalNumber, AI_AVG_TYRE_LIFE = ac.INIConfig.OptionalNumber, AI_ALTERNATE_LEVEL = ac.INIConfig.OptionalNumber,
-			--         PHYSICS_REBOOT = ac.INIConfig.OptionalNumber
-			--     }})
-			--     log("[Loaded] Applied config")
-			--     DRS_ENABLED_LAP = RARECONFIG.data.RULES.DRS_ACTIVATION_LAP
-			-- end
-			ui.newLine(1)
-		end)
+				ui.header("PIT STOPS")
+				slider(
+					RARECONFIG,
+					"RULES",
+					"RACE_REFUELING",
+					0,
+					1,
+					1,
+					true,
+					RARECONFIG.data.RULES.RACE_REFUELING == 1 and "Race Refueling: ENABLED"
+						or "Race Refueling: DISABLED",
+					"Enable or disable refueling during a race",
+					function(v)
+						return math.round(v, 0)
+					end
+				)
 
-		ui.tabItem("AI", ui.TabItemFlags.None, function()
-			ui.newLine(1)
+				ui.newLine(1)
+				-- ui.newLine(5)
 
-			ui.header("LEVEL")
-			slider(
-				RARECONFIG,
-				"AI",
-				"AI_ALTERNATE_LEVEL",
-				0,
-				1,
-				1,
-				true,
-				RARECONFIG.data.AI.AI_ALTERNATE_LEVEL == 1 and "Alternate AI Strength: ENABLED"
-					or "Alternate AI Strength: DISABLED",
-				"Changes the default AI level to be more competitive",
-				function(v)
-					return math.round(v, 0)
-				end
-			)
+				-- if ui.button("APPLY SETTINGS", vec2(ui.windowWidth()-40,25), ui.ButtonFlags.None) then
+				--     -- Load config file
+				--     RARECONFIG = MappedConfig(ac.getFolder(ac.FolderID.ACApps).."/lua/RARE/settings.ini", {
+				--         RULES = { DRS_RULES = ac.INIConfig.OptionalNumber, DRS_ACTIVATION_LAP = ac.INIConfig.OptionalNumber,
+				--         DRS_GAP_DELTA = ac.INIConfig.OptionalNumber, DRS_WET_DISABLE = ac.INIConfig.OptionalNumber,
+				--         VSC_RULES = ac.INIConfig.OptionalNumber, VSC_INIT_TIME = ac.INIConfig.OptionalNumber, VSC_DEPLOY_TIME = ac.INIConfig.OptionalNumber,
+				--         AI_FORCE_PIT_TYRES = ac.INIConfig.OptionalNumber, AI_AVG_TYRE_LIFE = ac.INIConfig.OptionalNumber, AI_ALTERNATE_LEVEL = ac.INIConfig.OptionalNumber,
+				--         PHYSICS_REBOOT = ac.INIConfig.OptionalNumber
+				--     }})
+				--     log("[Loaded] Applied config")
+				--     DRS_ENABLED_LAP = RARECONFIG.data.RULES.DRS_ACTIVATION_LAP
+				-- end
+				ui.newLine(1)
+			end)
 
-			ui.newLine(1)
+			ui.tabItem("AI", ui.TabItemFlags.None, function()
+				ui.newLine(1)
 
-			slider(
-				RARECONFIG,
-				"AI",
-				"AI_RELATIVE_SCALING",
-				0,
-				1,
-				1,
-				true,
-				RARECONFIG.data.AI.AI_RELATIVE_SCALING == 1 and "Relative AI Scaling: ENABLED"
-					or "Relative AI Scaling: DISABLED",
-				"Enables relative AI scaling",
-				function(v)
-					return math.round(v, 0)
-				end
-			)
-
-			slider(
-				RARECONFIG,
-				"AI",
-				"AI_RELATIVE_LEVEL",
-				70,
-				100,
-				1,
-				true,
-				RARECONFIG.data.AI.AI_RELATIVE_LEVEL == 1 and "Relative AI Level %.0f%%" or "Relative AI Level %.0f%%",
-				"Relative AI level, for easier scaling with BoP'd grids",
-				function(v)
-					FIRST_LAUNCH = false
-					initialize(sim)
-					return math.round(v, 0)
-				end
-			)
-
-			ui.newLine(1)
-
-			slider(
-				RARECONFIG,
-				"AI",
-				"AI_MGUK_CONTROL",
-				0,
-				1,
-				1,
-				true,
-				RARECONFIG.data.AI.AI_MGUK_CONTROL == 1 and "AI Dynamic MGUK: ENABLED" or "AI Dynamic MGUK: DISABLED",
-				"Enables AI to make MGUK changes during the race",
-				function(v)
-					return math.round(v, 0)
-				end
-			)
-
-			ui.newLine(1)
-
-			ui.header("TYRES")
-			slider(
-				RARECONFIG,
-				"AI",
-				"AI_FORCE_PIT_TYRES",
-				0,
-				1,
-				1,
-				true,
-				RARECONFIG.data.AI.AI_FORCE_PIT_TYRES == 1 and "Pit When Tyres Worn: ENABLED"
-					or "Pit When Tyres Worn: DISABLED",
-				"Force AI to pit for new tyres when their average tyre life is below AI TYRE LIFE",
-				function(v)
-					return math.round(v, 0)
-				end
-			)
-
-			ui.newLine(1)
-
-			local driver = DRIVERS[sim.focusedCar]
-			if RARECONFIG.data.AI.AI_FORCE_PIT_TYRES == 1 then
+				ui.header("LEVEL")
 				slider(
 					RARECONFIG,
 					"AI",
-					"AI_AVG_TYRE_LIFE",
+					"AI_ALTERNATE_LEVEL",
 					0,
+					1,
+					1,
+					true,
+					RARECONFIG.data.AI.AI_ALTERNATE_LEVEL == 1 and "Alternate AI Strength: ENABLED"
+						or "Alternate AI Strength: DISABLED",
+					"Changes the default AI level to be more competitive",
+					function(v)
+						return math.round(v, 0)
+					end
+				)
+
+				ui.newLine(1)
+
+				slider(
+					RARECONFIG,
+					"AI",
+					"AI_RELATIVE_SCALING",
+					0,
+					1,
+					1,
+					true,
+					RARECONFIG.data.AI.AI_RELATIVE_SCALING == 1 and "Relative AI Scaling: ENABLED"
+						or "Relative AI Scaling: DISABLED",
+					"Enables relative AI scaling",
+					function(v)
+						return math.round(v, 0)
+					end
+				)
+
+				slider(
+					RARECONFIG,
+					"AI",
+					"AI_RELATIVE_LEVEL",
+					70,
 					100,
 					1,
-					false,
-					"Pit Below Avg Tyre Life: %.2f%%",
-					"AI will pit after average tyre life % is below this value",
+					true,
+					RARECONFIG.data.AI.AI_RELATIVE_LEVEL == 1 and "Relative AI Level %.0f%%"
+						or "Relative AI Level %.0f%%",
+					"Relative AI level, for easier scaling with BoP'd grids",
 					function(v)
-						RARECONFIG.data.AI.AI_SINGLE_TYRE_LIFE =
-							math.clamp(RARECONFIG.data.AI.AI_SINGLE_TYRE_LIFE, 0, math.floor(v / 0.5 + 0.5) * 0.5)
-						return math.floor(v / 0.5 + 0.5) * 0.5
+						FIRST_LAUNCH = false
+						initialize(sim)
+						return math.round(v, 0)
 					end
 				)
 
+				if ac.getPatchVersionCode() >= 2278 then
+					ui.newLine(1)
+
+					slider(
+						RARECONFIG,
+						"AI",
+						"AI_MGUK_CONTROL",
+						0,
+						1,
+						1,
+						true,
+						RARECONFIG.data.AI.AI_MGUK_CONTROL == 1 and "AI Dynamic MGUK: ENABLED"
+							or "AI Dynamic MGUK: DISABLED",
+						"Enables AI to make MGUK changes during the race",
+						function(v)
+							return math.round(v, 0)
+						end
+					)
+				end
+
+				ui.newLine(1)
+
+				ui.header("TYRES")
 				slider(
 					RARECONFIG,
 					"AI",
-					"AI_AVG_TYRE_LIFE_RANGE",
+					"AI_FORCE_PIT_TYRES",
 					0,
-					15,
 					1,
-					false,
-					"Variability: %.2f%%",
-					"AI will pit if one tyre's life % is below this value",
+					1,
+					true,
+					RARECONFIG.data.AI.AI_FORCE_PIT_TYRES == 1 and "Pit When Tyres Worn: ENABLED"
+						or "Pit When Tyres Worn: DISABLED",
+					"Force AI to pit for new tyres when their average tyre life is below AI TYRE LIFE",
 					function(v)
-						return math.floor(v / 0.5 + 0.5) * 0.5
+						return math.round(v, 0)
 					end
 				)
 
 				ui.newLine(1)
 
-				slider(
-					RARECONFIG,
-					"AI",
-					"AI_SINGLE_TYRE_LIFE",
-					0,
-					RARECONFIG.data.AI.AI_AVG_TYRE_LIFE,
-					1,
-					false,
-					"Pit Below Single Tyre Life: %.2f%%",
-					"AI will pit if one tyre's life % is below this value",
-					function(v)
-						return math.floor(v / 0.5 + 0.5) * 0.5
-					end
-				)
-
-				slider(
-					RARECONFIG,
-					"AI",
-					"AI_SINGLE_TYRE_LIFE_RANGE",
-					0,
-					15,
-					1,
-					false,
-					"Variability: %.2f%%",
-					"AI will pit if one tyre's life % is below this value",
-					function(v)
-						return math.floor(v / 0.5 + 0.5) * 0.5
-					end
-				)
-
-				ui.newLine(1)
-
-				if not sim.isInMainMenu then
-					if driver.car.isAIControlled then
-						local buttonFlags = ui.ButtonFlags.None
-						if driver.aiPitting or driver.car.isInPitlane then
-							buttonFlags = ui.ButtonFlags.Disabled
+				local driver = DRIVERS[sim.focusedCar]
+				if RARECONFIG.data.AI.AI_FORCE_PIT_TYRES == 1 then
+					slider(
+						RARECONFIG,
+						"AI",
+						"AI_AVG_TYRE_LIFE",
+						0,
+						100,
+						1,
+						false,
+						"Pit Below Avg Tyre Life: %.2f%%",
+						"AI will pit after average tyre life % is below this value",
+						function(v)
+							RARECONFIG.data.AI.AI_SINGLE_TYRE_LIFE =
+								math.clamp(RARECONFIG.data.AI.AI_SINGLE_TYRE_LIFE, 0, math.floor(v / 0.5 + 0.5) * 0.5)
+							return math.floor(v / 0.5 + 0.5) * 0.5
 						end
-						if ui.button("FORCE FOCUSED AI TO PIT NOW", vec2(ui.windowWidth() - 77, 25), buttonFlags) then
-							physics.setAIPitStopRequest(driver.index, true)
-						end
+					)
 
-						-- if ui.button("Awaken Car", vec2(ui.windowWidth()-77,25), ui.ButtonFlags.None) then
-						--     driver.aiPrePitFuel = 140
-						--     physics.setCarFuel(driver.index,140)
-						--     physics.awakeCar(driver.index)
-						--     physics.setCarFuel(driver.index,140)
-						--     physics.resetCarState(driver.index)
-						--     physics.engageGear(driver.index,1)
-						--     physics.setEngineRPM(driver.index, 13000)
-						--     physics.setCarVelocity(driver.index,vec3(0,0,5))
-						-- end
-					else
-						if
-							ui.button("FORCE ALL AI TO PIT NOW", vec2(ui.windowWidth() - 77, 25), ui.ButtonFlags.None)
-						then
-							for i = 0, #DRIVERS do
-								local driver = DRIVERS[i]
-								if driver.car.isAIControlled then
-									physics.setAIPitStopRequest(driver.index, true)
+					slider(
+						RARECONFIG,
+						"AI",
+						"AI_AVG_TYRE_LIFE_RANGE",
+						0,
+						15,
+						1,
+						false,
+						"Variability: %.2f%%",
+						"AI will pit if one tyre's life % is below this value",
+						function(v)
+							return math.floor(v / 0.5 + 0.5) * 0.5
+						end
+					)
+
+					ui.newLine(1)
+
+					slider(
+						RARECONFIG,
+						"AI",
+						"AI_SINGLE_TYRE_LIFE",
+						0,
+						RARECONFIG.data.AI.AI_AVG_TYRE_LIFE,
+						1,
+						false,
+						"Pit Below Single Tyre Life: %.2f%%",
+						"AI will pit if one tyre's life % is below this value",
+						function(v)
+							return math.floor(v / 0.5 + 0.5) * 0.5
+						end
+					)
+
+					slider(
+						RARECONFIG,
+						"AI",
+						"AI_SINGLE_TYRE_LIFE_RANGE",
+						0,
+						15,
+						1,
+						false,
+						"Variability: %.2f%%",
+						"AI will pit if one tyre's life % is below this value",
+						function(v)
+							return math.floor(v / 0.5 + 0.5) * 0.5
+						end
+					)
+
+					ui.newLine(1)
+
+					if not sim.isInMainMenu and ac.getPatchVersionCode() >= 2278 then
+						if driver.car.isAIControlled then
+							local buttonFlags = ui.ButtonFlags.None
+							if driver.aiPitting or driver.car.isInPitlane then
+								buttonFlags = ui.ButtonFlags.Disabled
+							end
+							if
+								ui.button("FORCE FOCUSED AI TO PIT NOW", vec2(ui.windowWidth() - 77, 25), buttonFlags)
+							then
+								physics.setAIPitStopRequest(driver.index, true)
+							end
+
+							-- if ui.button("Awaken Car", vec2(ui.windowWidth()-77,25), ui.ButtonFlags.None) then
+							--     driver.aiPrePitFuel = 140
+							--     physics.setCarFuel(driver.index,140)
+							--     physics.awakeCar(driver.index)
+							--     physics.setCarFuel(driver.index,140)
+							--     physics.resetCarState(driver.index)
+							--     physics.engageGear(driver.index,1)
+							--     physics.setEngineRPM(driver.index, 13000)
+							--     physics.setCarVelocity(driver.index,vec3(0,0,5))
+							-- end
+						else
+							if
+								ui.button(
+									"FORCE ALL AI TO PIT NOW",
+									vec2(ui.windowWidth() - 77, 25),
+									ui.ButtonFlags.None
+								)
+							then
+								for i = 0, #DRIVERS do
+									local driver = DRIVERS[i]
+									if driver.car.isAIControlled then
+										physics.setAIPitStopRequest(driver.index, true)
+									end
 								end
 							end
-						end
 
-						-- if ui.button("TELEPORT ALL AI TO PIT NOW", vec2(ui.windowWidth()-77,25), ui.ButtonFlags.None) then
-						--     for i=0, #DRIVERS do
-						--         local driver = DRIVERS[i]
-						--         if driver.car.isAIControlled then
-						--             physics.teleportCarTo(driver.index,ac.SpawnSet.Pits)
-						--         end
-						--     end
-						-- end
+							-- if ui.button("TELEPORT ALL AI TO PIT NOW", vec2(ui.windowWidth()-77,25), ui.ButtonFlags.None) then
+							--     for i=0, #DRIVERS do
+							--         local driver = DRIVERS[i]
+							--         if driver.car.isAIControlled then
+							--             physics.teleportCarTo(driver.index,ac.SpawnSet.Pits)
+							--         end
+							--     end
+							-- end
+						end
 					end
 				end
-			end
 
-			ui.newLine(1)
-		end)
+				ui.newLine(1)
+			end)
+		end
 
 		ui.tabItem("AUDIO", ui.TabItemFlags.None, function()
 			ui.newLine(1)
