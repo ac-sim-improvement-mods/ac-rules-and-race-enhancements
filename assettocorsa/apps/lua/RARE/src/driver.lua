116be052391004f37c1498ae40bf9466e128a1b4
William Gawlik <william.gawlik@outlook.com>
2023-03-30 21:16:00 -0500

    Fixex version compatibility with CSP 1.79

--- assettocorsa/apps/lua/RARE/src/driver.lua
+++ assettocorsa/apps/lua/RARE/src/driver.lua
@@ -1,201 +1,195 @@
 ---@class Driver
 ---@param carIndex number
 ---@return Driver
-Driver = class('Driver', function(carIndex)
-    local index = carIndex
-    local car = ac.getCar(index)
-    local name = ac.getDriverName(index)
-    local lapsCompleted = car.lapCount
+Driver = class("Driver", function(carIndex)
+	local index = carIndex
+	local car = ac.getCar(index)
+	local name = ac.getDriverName(index)
+	local lapsCompleted = car.lapCount
 
-    local aiThrottleLimitBase = 1
-    local aiThrottleLimit = 1
-    local aiLevel = 1
-    local aiAggression = 0
-    local aiPrePitFuel = 0
-    local aiPitCall = false
-    local aiPitting = false
-    local aiSplineOffset = 0
-    local aiMoveAside = false
-    local aiSpeedUp = false
-    local aiMgukDelivery = 0
-    local aiMgukRecovery = 0
-    local aiBaseBrakeHint = 1
-    local aiBrakeHint = 1
+	local aiThrottleLimitBase = 1
+	local aiThrottleLimit = 1
+	local aiLevel = 1
+	local aiAggression = 0
+	local aiPrePitFuel = 0
+	local aiPitCall = false
+	local aiPitting = false
+	local aiSplineOffset = 0
+	local aiMoveAside = false
+	local aiSpeedUp = false
+	local aiMgukDelivery = 0
+	local aiMgukRecovery = 0
+	local aiBaseBrakeHint = 1
+	local aiBrakeHint = 1
 
-    local outLap = false
-    local flyingLap = false
-    local inLap = false
-    local inLapCount = 0
+	local outLap = false
+	local flyingLap = false
+	local inLap = false
+	local inLapCount = 0
 
-    local pitstopCount = 0
-    local pitstopTime = 0
-    local pitlane = false
-    local pitlaneTime = 0
-    local pitstop = false
-    local pitted = false
-    local lapPitted = 0
-    local pittedLaps = {}
-    local tyreLaps = 0
-    local tyreCompoundStart = car.compoundIndex
-    local tyreCompoundNext = car.compoundIndex
-    local tyreCompoundChange = false
-    local tyreCompoundsAvailable = {0}
-    local tyreStints = {}
-    local tyreCompoundMaterialTarget = ""
+	local pitstopCount = 0
+	local pitstopTime = 0
+	local pitlane = false
+	local pitlaneTime = 0
+	local pitstop = false
+	local pitted = false
+	local lapPitted = 0
+	local pittedLaps = {}
+	local tyreLaps = 0
+	local tyreCompoundStart = car.compoundIndex
+	local tyreCompoundNext = car.compoundIndex
+	local tyreCompoundChange = false
+	local tyreCompoundsAvailable = { 0 }
+	local tyreStints = {}
+	local tyreCompoundMaterialTarget = ""
 
-    local trackPosition = -1
-    local carAhead = -1
-    local carAheadDelta = -1
+	local trackPosition = -1
+	local carAhead = -1
+	local carAheadDelta = -1
 
-    local drsActivationZone = car.drsAvailable
-    local drsZoneNextId = 0
-    local drsZoneId = 0
-    local drsZonePrevId = 0
-    local drsCheck = false
-    local drsAvailable = false
-    local drsDeployable = false
-    local drsBeepFx = false
-    local drsFlapFx = false
+	local drsActivationZone = car.drsAvailable
+	local drsZoneNextId = 0
+	local drsZoneId = 0
+	local drsZonePrevId = 0
+	local drsCheck = false
+	local drsAvailable = false
+	local drsDeployable = false
+	local drsBeepFx = false
+	local drsFlapFx = false
 
-    local timePenalty = 0
-    local illegalOvertake = false
-    local returnRacePosition = -1
-    local returnPostionTimer = -1
+	local timePenalty = 0
+	local illegalOvertake = false
+	local returnRacePosition = -1
+	local returnPostionTimer = -1
 
-    local aiTyreAvgRandom = randomizer(index, RARECONFIG.data.AI
-                                           .AI_AVG_TYRE_LIFE_RANGE)
-    local aiTyreSingleRandom = randomizer(index, RARECONFIG.data.AI
-                                              .AI_SINGLE_TYRE_LIFE_RANGE)
+	local aiTyreAvgRandom = randomizer(index, RARECONFIG.data.AI.AI_AVG_TYRE_LIFE_RANGE)
+	local aiTyreSingleRandom = randomizer(index, RARECONFIG.data.AI.AI_SINGLE_TYRE_LIFE_RANGE)
 
-    log("[Loaded] Driver [" .. index .. "] " .. name)
+	log("[" .. index .. "] " .. name .. " loaded")
 
-    return {
-        tyreCompoundMaterialTarget = tyreCompoundMaterialTarget,
-        pittedLaps = pittedLaps,
-        tyreStints = tyreStints,
-        aiBrakeHint = aiBrakeHint,
-        aiBaseBrakeHint = aiBaseBrakeHint,
-        tyreCompoundsAvailable = tyreCompoundsAvailable,
-        tyreCompoundStart = tyreCompoundStart,
-        tyreCompoundNext = tyreCompoundNext,
-        tyreCompoundChange = tyreCompoundChange,
-        aiSplineOffset = aiSplineOffset,
-        aiSpeedUp = aiSpeedUp,
-        aiMoveAside = aiMoveAside,
-        inLapCount = inLapCount,
-        inLap = inLap,
-        flyingLap = flyingLap,
-        outLap = outLap,
-        aiThrottleLimitBase = aiThrottleLimitBase,
-        aiThrottleLimit = aiThrottleLimit,
-        pitlaneTime = pitlaneTime,
-        pitlane = pitlane,
-        pitstop = pitstop,
-        pitstopTime = pitstopTime,
-        pitted = pitted,
-        pitstopCount = pitstopCount,
-        tyreLaps = tyreLaps,
-        lapPitted = lapPitted,
-        drsBeepFx = drsBeepFx,
-        drsFlapFx = drsFlapFx,
-        drsZoneNextId = drsZoneNextId,
-        drsDeployable = drsDeployable,
-        drsZonePrevId = drsZonePrevId,
-        drsZoneId = drsZoneId,
-        drsActivationZone = drsActivationZone,
-        drsAvailable = drsAvailable,
-        drsCheck = drsCheck,
-        aiTyreSingleRandom = aiTyreSingleRandom,
-        aiTyreAvgRandom = aiTyreAvgRandom,
-        aiPitting = aiPitting,
-        aiPitCall = aiPitCall,
-        aiPrePitFuel = aiPrePitFuel,
-        aiLevel = aiLevel,
-        aiAggression = aiAggression,
-        returnPostionTimer = returnPostionTimer,
-        returnRacePosition = returnRacePosition,
-        timePenalty = timePenalty,
-        illegalOvertake = illegalOvertake,
-        carAheadDelta = carAheadDelta,
-        carAhead = carAhead,
-        trackPosition = trackPosition,
-        lapsCompleted = lapsCompleted,
-        index = index,
-        name = name,
-        car = car,
-        aiMgukDelivery = aiMgukDelivery,
-        aiMgukRecovery = aiMgukRecovery
-    }
+	return {
+		tyreCompoundMaterialTarget = tyreCompoundMaterialTarget,
+		pittedLaps = pittedLaps,
+		tyreStints = tyreStints,
+		aiBrakeHint = aiBrakeHint,
+		aiBaseBrakeHint = aiBaseBrakeHint,
+		tyreCompoundsAvailable = tyreCompoundsAvailable,
+		tyreCompoundStart = tyreCompoundStart,
+		tyreCompoundNext = tyreCompoundNext,
+		tyreCompoundChange = tyreCompoundChange,
+		aiSplineOffset = aiSplineOffset,
+		aiSpeedUp = aiSpeedUp,
+		aiMoveAside = aiMoveAside,
+		inLapCount = inLapCount,
+		inLap = inLap,
+		flyingLap = flyingLap,
+		outLap = outLap,
+		aiThrottleLimitBase = aiThrottleLimitBase,
+		aiThrottleLimit = aiThrottleLimit,
+		pitlaneTime = pitlaneTime,
+		pitlane = pitlane,
+		pitstop = pitstop,
+		pitstopTime = pitstopTime,
+		pitted = pitted,
+		pitstopCount = pitstopCount,
+		tyreLaps = tyreLaps,
+		lapPitted = lapPitted,
+		drsBeepFx = drsBeepFx,
+		drsFlapFx = drsFlapFx,
+		drsZoneNextId = drsZoneNextId,
+		drsDeployable = drsDeployable,
+		drsZonePrevId = drsZonePrevId,
+		drsZoneId = drsZoneId,
+		drsActivationZone = drsActivationZone,
+		drsAvailable = drsAvailable,
+		drsCheck = drsCheck,
+		aiTyreSingleRandom = aiTyreSingleRandom,
+		aiTyreAvgRandom = aiTyreAvgRandom,
+		aiPitting = aiPitting,
+		aiPitCall = aiPitCall,
+		aiPrePitFuel = aiPrePitFuel,
+		aiLevel = aiLevel,
+		aiAggression = aiAggression,
+		returnPostionTimer = returnPostionTimer,
+		returnRacePosition = returnRacePosition,
+		timePenalty = timePenalty,
+		illegalOvertake = illegalOvertake,
+		carAheadDelta = carAheadDelta,
+		carAhead = carAhead,
+		trackPosition = trackPosition,
+		lapsCompleted = lapsCompleted,
+		index = index,
+		name = name,
+		car = car,
+		aiMgukDelivery = aiMgukDelivery,
+		aiMgukRecovery = aiMgukRecovery,
+	}
 end, class.NoInitialize)
 
 --- Returns lap pitted or lap count if driver just pitted
 ---@param driver Driver
 ---@return number
 local function getLapPitted(driver)
-    return (driver.tyreLaps > 0 and driver.car.isInPitlane) and
-               driver.car.lapCount or driver.lapPitted
+	return (driver.tyreLaps > 0 and driver.car.isInPitlane) and driver.car.lapCount or driver.lapPitted
 end
 
 --- Returns tyre lap count
 ---@param driver Driver
 ---@return number
 local function getTyreLapCount(driver)
-    return (driver.car.isInPitlane and not driver.pitted) and driver.tyreLaps or
-               (driver.car.lapCount - driver.lapPitted)
+	return (driver.car.isInPitlane and not driver.pitted) and driver.tyreLaps
+		or (driver.car.lapCount - driver.lapPitted)
 end
 
 local function getPitstopCount(driver)
-    if driver.car.isInPit and not driver.pitted then
-        driver.pitted = true
-        driver.aiTyreAvgRandom = randomizer(driver.index, RARECONFIG.data.AI
-                                                .AI_AVG_TYRE_LIFE_RANGE)
-        driver.aiTyreSingleRandom = randomizer(driver.index, RARECONFIG.data.AI
-                                                   .AI_SINGLE_TYRE_LIFE_RANGE)
-        return driver.pitstopCount + 1
-    elseif not driver.car.isInPitlane and driver.pitted then
-        driver.pitted = false
-    end
+	if driver.car.isInPit and not driver.pitted then
+		driver.pitted = true
+		driver.aiTyreAvgRandom = randomizer(driver.index, RARECONFIG.data.AI.AI_AVG_TYRE_LIFE_RANGE)
+		driver.aiTyreSingleRandom = randomizer(driver.index, RARECONFIG.data.AI.AI_SINGLE_TYRE_LIFE_RANGE)
+		return driver.pitstopCount + 1
+	elseif not driver.car.isInPitlane and driver.pitted then
+		driver.pitted = false
+	end
 
-    return driver.pitstopCount
+	return driver.pitstopCount
 end
 
 local function getPitTime(dt, driver)
-    if driver.car.isInPitlane then
-        if driver.pitlaneTime > 0 and not driver.pitlane then
-            driver.pitlane = true
-            return 0
-        else
-            return driver.pitlaneTime + dt
-        end
-    else
-        driver.pitlane = false
-        return driver.pitlaneTime
-    end
+	if driver.car.isInPitlane then
+		if driver.pitlaneTime > 0 and not driver.pitlane then
+			driver.pitlane = true
+			return 0
+		else
+			return driver.pitlaneTime + dt
+		end
+	else
+		driver.pitlane = false
+		return driver.pitlaneTime
+	end
 end
 
 local function getPitstopTime(dt, driver)
-    if driver.car.isInPit then
-        if not driver.pitstop then
-            driver.pitstop = true
-            return 0
-        else
-            return driver.pitstopTime + dt
-        end
-    else
-        driver.pitstop = false
-        return driver.pitstopTime
-    end
+	if driver.car.isInPit then
+		if not driver.pitstop then
+			driver.pitstop = true
+			return 0
+		else
+			return driver.pitstopTime + dt
+		end
+	else
+		driver.pitstop = false
+		return driver.pitstopTime
+	end
 end
 
 function Driver:update(dt, sim)
-    self.lapPitted = getLapPitted(self)
-    self.tyreLaps = getTyreLapCount(self)
-    self.pitstopCount = getPitstopCount(self)
-    self.pitlaneTime = getPitTime(dt, self)
-    self.pitstopTime = getPitstopTime(dt, self)
+	self.lapPitted = getLapPitted(self)
+	self.tyreLaps = getTyreLapCount(self)
+	self.pitstopCount = getPitstopCount(self)
+	self.pitlaneTime = getPitTime(dt, self)
+	self.pitstopTime = getPitstopTime(dt, self)
 
-    if self.carAhead >= 0 then
-        self.carAheadDelta = getDelta(sim, self.index, self.carAhead)
-    end
+	if self.carAhead >= 0 then
+		self.carAheadDelta = getDelta(sim, self.index, self.carAhead)
+	end
 end
-
--- assettocorsa/apps/lua/RARE/src/rc.lua
+++ assettocorsa/apps/lua/RARE/src/rc.lua
@@ -1,24 +1,24 @@
 ---@diagnostic disable: return-type-mismatch
-local connect = require 'rare/connection'
-local drs = require 'src/controllers/drs'
-local vsc = require 'src/controllers/vsc'
-local ai = require 'src/controllers/ai'
-local notifications = require 'src/ui/notifications'
+local connect = require("rare/connection")
+local drs = require("src/controllers/drs")
+local vsc = require("src/controllers/vsc")
+local ai = require("src/controllers/ai")
+local notifications = require("src/ui/notifications")
 
 local rc = {}
 
 DRIVERS = {}
 
 function readOnly(t)
-    local proxy = {}
-    local mt = {
-        __index = t,
-        __newindex = function(t, k, v)
-            error("attempt to update a read-only value", 2)
-        end
-    }
-    setmetatable(proxy, mt)
-    return proxy
+	local proxy = {}
+	local mt = {
+		__index = t,
+		__newindex = function(t, k, v)
+			error("attempt to update a read-only value", 2)
+		end,
+	}
+	setmetatable(proxy, mt)
+	return proxy
 end
 
 ---@alias rc.WeekendSessions
@@ -30,110 +30,108 @@ end
 ---| `rc.WeekendSessions.Q3` @Value: 5.
 ---| `rc.WeekendSessions.Race` @Value: 6.
 rc.WeekendSessions = {
-    FP1 = 0,
-    FP2 = 1,
-    FP3 = 2,
-    Q1 = 3,
-    Q2 = 4,
-    Q3 = 5,
-    Race = 6
+	FP1 = 0,
+	FP2 = 1,
+	FP3 = 2,
+	Q1 = 3,
+	Q2 = 4,
+	Q3 = 5,
+	Race = 6,
 }
 
 ---Returns leaders completed lap count.
 ---@return lapCount number
 local function getLeaderCompletedLaps(sim)
-    for i = 0, sim.carsCount - 1 do
-        local car = ac.getCar(i)
+	for i = 0, sim.carsCount - 1 do
+		local car = ac.getCar(i)
 
-        if car.racePosition == 1 then return car.lapCount end
-    end
+		if car.racePosition == 1 then
+			return car.lapCount
+		end
+	end
 end
 
 ---Returns whether DRS is enabled or not
 ---@param config RARECONFIG.data
 ---@return drsEnabled boolean
 local function isDrsEnabled(config, leaderCompletedLaps)
-    local drsActivationLap = config.RULES.DRS_ACTIVATION_LAP
-    if leaderCompletedLaps + 1 >= drsActivationLap then
-        return true, drsActivationLap
-    else
-        return false, drsActivationLap
-    end
+	local drsActivationLap = config.RULES.DRS_ACTIVATION_LAP
+	if leaderCompletedLaps + 1 >= drsActivationLap then
+		return true, drsActivationLap
+	else
+		return false, drsActivationLap
+	end
 end
 
 ---Returns whether the track is too wet for DRS enabled or not
 ---@param config RARECONFIG.data
 ---@return wetTrack boolean
 local function isTrackWet(config, sim)
-    local wet_limit = config.RULES.DRS_WET_LIMIT / 100
-    local track_wetness = sim.rainWetness
-    local track_puddles = sim.rainWater
+	local track_wetness = sim.rainWetness
+	local track_puddles = sim.rainWater
+	local total_wetness = (track_wetness + (track_puddles * 10)) * 1000
 
-    local total_wetness = ((track_wetness / 5) + (track_puddles * 10)) / 2
-
-    if total_wetness >= wet_limit then
-        return true
-    else
-        return false
-    end
+	if total_wetness >= 100 then
+		return true
+	else
+		return false
+	end
 end
 
 --- Sets the on track order of drivers
 --- and excludes drivers in the pitlane
 --- @param drivers DRIVERS
 local function getTrackOrder(drivers)
-    local trackOrder = {}
-    for index = 0, #drivers do
-        if not drivers[index].car.isInPitlane then
-            table.insert(trackOrder, drivers[index])
-        else
-            drivers[index].trackPosition = -1
-        end
-    end
+	local trackOrder = {}
+	for index = 0, #drivers do
+		if not drivers[index].car.isInPitlane then
+			table.insert(trackOrder, drivers[index])
+		else
+			drivers[index].trackPosition = -1
+		end
+	end
 
-    -- Sort drivers by position on track, and ignore drivers in the pits
-    table.sort(trackOrder, function(a, b)
-        return a.car.splinePosition > b.car.splinePosition
-    end)
+	-- Sort drivers by position on track, and ignore drivers in the pits
+	table.sort(trackOrder, function(a, b)
+		return a.car.splinePosition > b.car.splinePosition
+	end)
 
-    for trackPos = 1, #trackOrder do
-        local index = trackOrder[trackPos].index
-        drivers[index].trackPosition = trackPos
+	for trackPos = 1, #trackOrder do
+		local index = trackOrder[trackPos].index
+		drivers[index].trackPosition = trackPos
 
-        if trackPos == 1 then
-            drivers[index].carAhead = trackOrder[#trackOrder].index
-        else
-            drivers[index].carAhead = trackOrder[trackPos - 1].index
-        end
-    end
+		if trackPos == 1 then
+			drivers[index].carAhead = trackOrder[#trackOrder].index
+		else
+			drivers[index].carAhead = trackOrder[trackPos - 1].index
+		end
+	end
 
-    return #trackOrder
+	return #trackOrder
 end
 
 local function setAIFuelTankMax(sim, driver)
-    local fuelcons = ac.INIConfig.carData(driver.index, 'fuel_cons.ini'):get(
-                         'FUEL_EVAL', 'KM_PER_LITER', 0.0)
-    local fuelload = 0
-    local fuelPerLap = (sim.trackLengthM / 1000) / (fuelcons - (fuelcons * 0.1))
+	local fuelcons = ac.INIConfig.carData(driver.index, "fuel_cons.ini"):get("FUEL_EVAL", "KM_PER_LITER", 0.0)
+	local fuelload = 0
+	local fuelPerLap = (sim.trackLengthM / 1000) / (fuelcons - (fuelcons * 0.1))
 
-    if sim.raceSessionType == ac.SessionType.Race then
-        fuelload = ((ac.getSession(sim.currentSessionIndex).laps + 2) *
-                       fuelPerLap)
-    elseif sim.raceSessionType == ac.SessionType.Qualify then
-        fuelload = 3.5 * fuelPerLap
-    end
+	if sim.raceSessionType == ac.SessionType.Race then
+		fuelload = ((ac.getSession(sim.currentSessionIndex).laps + 2) * fuelPerLap)
+	elseif sim.raceSessionType == ac.SessionType.Qualify then
+		fuelload = 3.5 * fuelPerLap
+	end
 
-    physics.setCarFuel(driver.index, fuelload)
+	physics.setCarFuel(driver.index, fuelload)
 end
 
 --- Race Control for qualify sessions
 --- @param config RARECONFIG.data
 --- @param driver Driver
 local function qualifySession(racecontrol, config, driver)
-    if racecontrol.sim.sessionTimeLeft <= 0 then
-        physics.setAIPitStopRequest(driver.index, false)
-        ac.log('sessionover')
-    end
+	if racecontrol.sim.sessionTimeLeft <= 0 then
+		physics.setAIPitStopRequest(driver.index, false)
+		ac.log("sessionover")
+	end
 end
 
 --- Race Control for practice sessions
@@ -145,40 +143,45 @@ local function practiceSession(racecontrol, config, driver) end
 --- @param config RARECONFIG.data
 --- @param driver Driver
 local function raceSession(lastUpdate, racecontrol, config, driver)
-    local raceRules = config.RULES
-    local aiRules = config.AI
+	local raceRules = config.RULES
+	local aiRules = config.AI
 
-    if driver.car.isAIControlled then
-        if racecontrol.sim.isInMainMenu then
-            setAIFuelTankMax(racecontrol.sim, driver)
-            physics.setAIPitStopRequest(driver.index, false)
-        end
-    end
+	if driver.car.isAIControlled then
+		if racecontrol.sim.isInMainMenu then
+			setAIFuelTankMax(racecontrol.sim, driver)
 
-    -- notifications handler
-    if lastUpdate then
-        if not lastUpdate.drsEnabled and racecontrol.drsEnabled then
-            notifications.popup("DRS ENABLED")
-        end
+			if ac.getPatchVersionCode() >= 2278 then
+				physics.setAIPitStopRequest(driver.index, false)
+			end
+		end
+	end
 
-        if not lastUpdate.wetTrack and racecontrol.wetTrack then
-            notifications.popup("DRS DISABLED - WET TRACK")
-        end
+	-- notifications handler
+	if lastUpdate then
+		if not lastUpdate.drsEnabled and racecontrol.drsEnabled then
+			notifications.popup("DRS ENABLED")
+		end
 
-        if lastUpdate.wetTrack and not racecontrol.wetTrack then
-            notifications.popup("DRS ENABLED IN 2 LAPS")
-        end
-    end
+		if not lastUpdate.wetTrack and racecontrol.wetTrack then
+			notifications.popup("DRS DISABLED - WET TRACK")
+		end
 
-    if raceRules.DRS_RULES == 1 then
-        drs.controller(racecontrol.sim, driver, racecontrol.drsEnabled)
-    else
-        driver.drsAvailable = true
-    end
+		if lastUpdate.wetTrack and not racecontrol.wetTrack then
+			notifications.popup("DRS ENABLED IN 2 LAPS")
+		end
+	end
 
-    if driver.car.isAIControlled then ai.controller(aiRules, driver) end
+	if raceRules.DRS_RULES == 1 then
+		drs.controller(racecontrol.sim, driver, racecontrol.drsEnabled)
+	else
+		driver.drsAvailable = true
+	end
 
-    return driver
+	if driver.car.isAIControlled then
+		ai.controller(aiRules, driver)
+	end
+
+	return driver
 end
 
 --- Switch for runnimg the different kinds of sessioms
@@ -186,40 +189,40 @@ end
 --- @param config RARECONFIG.data
 --- @param driver Driver
 local function runSession(lastUpdate, racecontrol, sessionType, driver)
-    local config = RARECONFIG.data
+	local config = RARECONFIG.data
 
-    if sessionType == ac.SessionType.Race then
-        raceSession(lastUpdate, racecontrol, config, driver)
-    elseif sessionType == ac.SessionType.Qualify then
-        qualifySession(racecontrol, config, driver)
-    elseif sessionType == ac.SessionType.Practice then
-        practiceSession(racecontrol, config, driver)
-    end
+	if sessionType == ac.SessionType.Race then
+		raceSession(lastUpdate, racecontrol, config, driver)
+	elseif sessionType == ac.SessionType.Qualify then
+		qualifySession(racecontrol, config, driver)
+	elseif sessionType == ac.SessionType.Practice then
+		practiceSession(racecontrol, config, driver)
+	end
 
-    return driver
+	return driver
 end
 
 local function update(sim, drivers)
-    local config = RARECONFIG.data
-    local carsOnTrackCount = getTrackOrder(drivers)
-    local leaderCompletedLaps = getLeaderCompletedLaps(sim)
-    local drsEnabled, drsEnabledLap = isDrsEnabled(config, leaderCompletedLaps)
-    local wetTrack = isTrackWet(config, sim)
-    local session = nil
+	local config = RARECONFIG.data
+	local carsOnTrackCount = getTrackOrder(drivers)
+	local leaderCompletedLaps = getLeaderCompletedLaps(sim)
+	local drsEnabled, drsEnabledLap = isDrsEnabled(config, leaderCompletedLaps)
+	local wetTrack = isTrackWet(config, sim)
+	local session = nil
 
-    if not sim.isOnlineRace then
-        session = ac.getSession(sim.currentSessionIndex)
-    end
+	if not sim.isOnlineRace then
+		session = ac.getSession(sim.currentSessionIndex)
+	end
 
-    return readOnly {
-        sim = sim,
-        session = session,
-        carsOnTrackCount = carsOnTrackCount,
-        leaderCompletedLaps = leaderCompletedLaps,
-        drsEnabled = drsEnabled,
-        drsEnabledLap = drsEnabledLap,
-        wetTrack = wetTrack
-    }
+	return readOnly({
+		sim = sim,
+		session = session,
+		carsOnTrackCount = carsOnTrackCount,
+		leaderCompletedLaps = leaderCompletedLaps,
+		drsEnabled = drsEnabled,
+		drsEnabledLap = drsEnabledLap,
+		wetTrack = wetTrack,
+	})
 end
 
 local racecontrol = nil
@@ -227,21 +230,20 @@ local racecontrol = nil
 --- Updates and returns race control variables
 --- @return racecontrol rc
 function rc.getRaceControl(dt, sim)
-    local drivers = DRIVERS
-    local lastUpdate = racecontrol
-    racecontrol = update(sim, drivers)
+	local drivers = DRIVERS
+	local lastUpdate = racecontrol
+	racecontrol = update(sim, drivers)
 
-    for i = 0, #drivers do
-        local driver = drivers[i]
-        driver:update(dt, sim)
-        DRIVERS[i] = runSession(lastUpdate, racecontrol, sim.raceSessionType,
-                                driver)
-        connect.storeDriverData(driver)
-    end
+	for i = 0, #drivers do
+		local driver = drivers[i]
+		driver:update(dt, sim)
+		DRIVERS[i] = runSession(lastUpdate, racecontrol, sim.raceSessionType, driver)
+		connect.storeDriverData(driver)
+	end
 
-    connect.storeRaceControlData(racecontrol)
+	connect.storeRaceControlData(racecontrol)
 
-    return racecontrol
+	return racecontrol
 end
 
 return rc
