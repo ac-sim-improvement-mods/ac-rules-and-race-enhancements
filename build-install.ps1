40b942baa4f6145ca149383f526c83b9bc61744c
William Gawlik <william.gawlik@outlook.com>
2023-04-18 00:12:18 +0000

    Further upated build script

--- build-install.ps1
+++ build-install.ps1
@@ -1,17 +1,19 @@
+$app_dir = "$PSScriptRoot\assettocorsa\apps\lua\RARE"
+$app = "$app_dir\app.lua"
 $build_dir = "$PSScriptRoot\build"
-$build_ver = ((Get-Content $PSScriptRoot\assettocorsa\apps\lua\RARE\app.lua)[2] -split "`"")[1]
-$build_code = ((Get-Content $PSScriptRoot\assettocorsa\apps\lua\RARE\app.lua)[3] -split "= ")[1]
+$build_ver = ((Get-Content $app)[2] -split "`"")[1]
+$build_code = ((Get-Content $app)[3] -split "= ")[1]
 $build_code = [int]$build_code + 1
 $build_code = [string]$build_code
 $build_ver = $build_code[0] + "." + $build_code[1] + "." + $build_code[2] + "." + $build_code[3] + ".preview" + $build_code[4]
 
 $date = Get-Date -Format "yyyy-MM-dd"
 
-(Get-Content "$PSScriptRoot\assettocorsa\apps\lua\RARE\app.lua") | ForEach-Object { $_ -replace "SCRIPT_VERSION =.+", "SCRIPT_VERSION = `"$build_ver`"" } | Set-Content "$PSScriptRoot\assettocorsa\apps\lua\RARE\app.lua"
-(Get-Content "$PSScriptRoot\assettocorsa\apps\lua\RARE\manifest.ini") | ForEach-Object { $_ -replace "VERSION =.+", "VERSION = $build_ver" } | Set-Content "$PSScriptRoot\assettocorsa\apps\lua\RARE\manifest.ini"
-(Get-Content "$PSScriptRoot\assettocorsa\apps\lua\RARE\app.lua") | ForEach-Object { $_ -replace "
-SCRIPT_BUILD_DATE =.+", "SCRIPT_BUILD_DATE = `"$date`"" } | Set-Content "$PSScriptRoot\assettocorsa\apps\lua\RARE\app.lua"
-(Get-Content "$PSScriptRoot\assettocorsa\apps\lua\RARE\app.lua") | ForEach-Object { $_ -replace "SCRIPT_VERSION_CODE =.+", "SCRIPT_VERSION_CODE = $build_code" } | Set-Content "$PSScriptRoot\assettocorsa\apps\lua\RARE\app.lua"
+(Get-Content $app) | ForEach-Object { $_ -replace "SCRIPT_VERSION =.+", "SCRIPT_VERSION = `"$build_ver`"" } | Set-Content $app
+(Get-Content "$app_dir\manifest.ini") | ForEach-Object { $_ -replace "VERSION =.+", "VERSION = $build_ver" } | Set-Content "$app_dir\manifest.ini"
+(Get-Content $app) | ForEach-Object { $_ -replace "
+SCRIPT_BUILD_DATE =.+", "SCRIPT_BUILD_DATE = `"$date`"" } | Set-Content $app
+(Get-Content $app) | ForEach-Object { $_ -replace "SCRIPT_VERSION_CODE =.+", "SCRIPT_VERSION_CODE = $build_code" } | Set-Content $app
 
 $target_file = "$build_dir\RARE_$build_ver.zip"
 if (Test-Path $target_file) {
