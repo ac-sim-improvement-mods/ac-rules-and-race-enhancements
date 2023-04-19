1b298f89b653846a8959d1dc04f1c4116a4814de
William Gawlik <william.gawlik@outlook.com>
2023-04-18 22:24:07 -0500

    Changed to 7zFM to get better compression than the PS module

--- build-install.ps1
+++ build-install.ps1
@@ -1,7 +1,13 @@
+$7zipPath = "$env:ProgramFiles\7-Zip\7z.exe"
+if (-not (Test-Path -Path $7zipPath -PathType Leaf)) {
+  throw "7 zip file '$7zipPath' not found"
+}
+Set-Alias Compress-7ZIPPER $7zipPath
+
 $app_dir = "$PSScriptRoot\RARE"
 $version = "$app_dir\app\version.lua"
+
 $build_dir = "$PSScriptRoot\build"
-$build_stage = "$build_dir\stage\RARE"
 $build_ver = ((Get-Content $version)[2] -split "`"")[1]
 $build_code = ((Get-Content $version)[3] -split "= ")[1]
 $build_code = [int]$build_code + 1
@@ -16,11 +22,11 @@ $date = Get-Date -Format "yyyy-MM-dd"
 SCRIPT_BUILD_DATE =.+", "SCRIPT_BUILD_DATE = `"$date`"" } | Set-Content $version
 (Get-Content $version) | ForEach-Object { $_ -replace "SCRIPT_VERSION_CODE =.+", "SCRIPT_VERSION_CODE = $build_code" } | Set-Content $version
 
-Copy-Item $app_dir -Destination $build_stage -Recurse -Force
-$target_file = "$build_dir\RARE_$build_ver.zip"
+$target_file = "$build_dir\RARE_$build_ver.7z"
 if (Test-Path $target_file) {
   Remove-Item $target_file
 }
-Compress-Archive -Path $build_dir\stage\RARE* -DestinationPath $target_file
+
+Compress-7ZIPPER a -mx=9 $target_file $app_dir
 
 & "$PSScriptRoot\install.ps1"
