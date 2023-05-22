971ae237fbe92dd5083ab55e23224e75ba68fc58
William Gawlik <william.gawlik@outlook.com>
2023-05-22 14:19:04 -0500

    Added 3 banner types

--- RARE/src/ui/windows/notification_window.lua
+++ RARE/src/ui/windows/notification_window.lua
@@ -5,79 +5,29 @@ local notifications = {}
 local notificationTimer = 0
 local notificationText = ""
 
-local raceControlBlockImage = ui.decodeImage(io.loadFromZip(ac.findFile("assets\\img\\img.zip"), "fia_block.png"))
-local raceControlBlockImageSize = ui.imageSize(raceControlBlockImage)
-local raceControlBannerImage = ui.decodeImage(io.loadFromZip(ac.findFile("assets\\img\\img.zip"), "fia_banner.png"))
-local raceControlBannerImageSize = ui.imageSize(raceControlBannerImage)
+local bannerHeight = 95
+local bannerDrsEnabled = ui.decodeImage(io.loadFromZip(ac.findFile("assets\\img.zip"), "banner_drs_enabled.png"))
+local bannerDrsEnabledWidth = 512.4
+local bannerDrsEnabledLaps =
+	ui.decodeImage(io.loadFromZip(ac.findFile("assets\\img.zip"), "banner_drs_enabled_laps.png"))
+local bannerDrsEnabledLapsWidth = 783.9
+local bannerDrsDisabled = ui.decodeImage(io.loadFromZip(ac.findFile("assets\\img.zip"), "banner_drs_disabled.png"))
+local bannerDrsDisabledWidth = 881.1
 
 local function drawRaceControl(text)
 	ui.beginScale()
-	ui.setCursorX(1700)
-	ui.setCursorY(200)
-	ui.image(raceControlBlockImage, vec2(100, 75), true)
 	ui.setCursorX(1800)
 	ui.setCursorY(200)
-	ui.image(raceControlBannerImage, vec2(750, 75))
+
+	if text == "DRS ENABLED" or text == "RACE CONTROL BANNER" then
+		ui.image(bannerDrsEnabled, vec2(bannerDrsEnabledWidth, bannerHeight))
+	elseif text == "DRS DISABLED - WET TRACK" then
+		ui.image(bannerDrsDisabled, vec2(bannerDrsDisabledWidth, bannerHeight))
+	elseif text == "DRS ENABLED IN 2 LAPS" then
+		ui.image(bannerDrsEnabledLaps, vec2(bannerDrsEnabledLapsWidth, bannerHeight))
+	end
+
 	ui.endScale(RARE_CONFIG.data.NOTIFICATIONS.SCALE)
-
-	-- ui.beginScale()
-	-- ui.pushDWriteFont("Formula1 Display;Weight=Bold")
-	-- -- ui.pushDWriteFont("Formula1 Display Bold Bold:fonts/f1.ttf")
-
-	-- local leftAlign = 147
-	-- local yAlign = -145
-	-- local fontSize = 32
-	-- local bannerHeight = 95
-	-- local bannerWidth = ui.measureDWriteText(text, fontSize * 0.8125).x + 40
-
-	-- -- Race Control dark blue rect
-	-- ui.drawRectFilled(vec2(0, 0), vec2(360, bannerHeight), rgbm(0.07, 0.12, 0.23, 1))
-
-	-- -- Information white rect
-	-- ui.drawRectFilled(vec2(360, 0), vec2(360 + bannerWidth, bannerHeight), rgbm(1, 1, 1, 1))
-
-	-- ui.drawText({
-	-- 	string = "RACE",
-	-- 	fontSize = fontSize,
-	-- 	xPos = leftAlign,
-	-- 	yPos = yAlign,
-	-- 	xAlign = ui.Alignment.Start,
-	-- 	yAlign = ui.Alignment.Center,
-	-- 	color = rgbm(1, 1, 1, 1),
-	-- })
-
-	-- ui.drawText({
-	-- 	string = "CONTROL",
-	-- 	fontSize = fontSize,
-	-- 	xPos = leftAlign,
-	-- 	yPos = yAlign + 35,
-	-- 	xAlign = ui.Alignment.Start,
-	-- 	yAlign = ui.Alignment.Center,
-	-- 	color = rgbm(1, 1, 1, 1),
-	-- })
-
-	-- ui.drawText({
-	-- 	string = text,
-	-- 	fontSize = fontSize * 0.8125,
-	-- 	xPos = 110 + bannerWidth / 2,
-	-- 	yPos = yAlign + 21,
-	-- 	xAlign = ui.Alignment.Center,
-	-- 	yAlign = ui.Alignment.Center,
-	-- 	color = rgbm(0.07, 0.12, 0.23, 1),
-	-- 	margin = vec2(500, 350),
-	-- })
-
-	-- ui.beginScale()
-	-- local pos_x = 7
-	-- local pos_y = -2
-	-- local size_x = pos_x + 146
-	-- local size_y = pos_y + 100
-
-	-- ui.drawImage("assets/icons/fia_logo.png", vec2(pos_x, pos_y), vec2(size_x, size_y), rgbm(1, 1, 1, 1), true)
-	-- ui.endScale(0.60)
-
-	-- ui.popDWriteFont()
-	-- ui.endScale(RARE_CONFIG.data.NOTIFICATIONS.SCALE)
 end
 
 local function drawNotification()
