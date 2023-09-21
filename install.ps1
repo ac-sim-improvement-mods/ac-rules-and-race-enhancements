bd0bf0a042929eb325b4cc01572e9abe2ce6a4bd
William Gawlik <william.gawlik@outlook.com>
2023-09-21 09:32:13 -0500

    Updated isntall

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
 $app_dir = "$PSScriptRoot\RARE"
-$ac_app_dir = "E:\Games\Steam\steamapps\common\assettocorsa\apps\lua"
+$ac_app_dir = "G:\Games\Steam\steamapps\common\assettocorsa\apps\lua"
 
 Copy-Item $app_dir -Destination $ac_app_dir -Recurse -Force
